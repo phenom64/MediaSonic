afd7c959981a1c1ccfb131bf4e263a99158bbf6b
Kavish Krishnakumar <75783341+phenom64@users.noreply.github.com>
2025-07-04 18:10:48 +0100

    Create application.h

---
+++ src/application.h
@@ -0,0 +1,41 @@
+/*
+ * TM & (C) 2025 Syndromatic Ltd. All rights reserved.
+ * Designed by Kavish Krishnakumar in Manchester.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <QApplication>
+#include <QLocalServer>
+#include <QLocalSocket>
+
+class Application : public QApplication
+{
+    Q_OBJECT
+public:
+    Application(int &argc, char *argv[]);
+    bool isRunning();
+
+private:
+    bool _isRunning;
+    QLocalServer *localServer;
+};
+
+#endif // APPLICATION_H
