94cc79c4473869465f24befc5bc33cc95886c0e3
Kavish Krishnakumar <75783341+phenom64@users.noreply.github.com>
2025-07-04 18:10:07 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * TM & (C) 2025 Syndromatic Ltd. All rights reserved.
+ * Designed by Kavish Krishnakumar in Manchester.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "application.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    Application app(argc, argv);
+
+    // Ensure only one instance of the application is running
+    if (app.isRunning()) {
+        return 0;
+    }
+
+    MainWindow win;
+    win.show();
+
+    return app.exec();
+}
