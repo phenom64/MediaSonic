9542d5a7a8d40372650348ff6bca591a7a28e320
Kavish Krishnakumar <75783341+phenom64@users.noreply.github.com>
2025-07-07 02:58:42 +0100

    Delete build directory

--- build/CMakeFiles/MediaSonic.dir/MediaSonic_autogen/UVLADIE3JM/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MediaSonic.dir/MediaSonic_autogen/UVLADIE3JM/qrc_resources.cpp.o: \
- /home/phenom/MediaSonic/build/MediaSonic_autogen/UVLADIE3JM/qrc_resources.cpp \
- /usr/include/stdc-predef.h
